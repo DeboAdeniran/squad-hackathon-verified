e2694c1d494d8a2003d20b70ed62308ad07b92be
github_DeboAdeniran <raqibadeniran03@gmail.com>
2026-05-11 03:07:17 +0100

    feat: add login and register dto

---
+++ verified-backend/src/main/java/com/verified/dto/response/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.verified.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class AuthResponse {
+    private UUID userId;
+    private String email;
+    private String fullName;
+    private String role;
+    private String message;
+}
