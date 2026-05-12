c51edd22ed53760bff59aff34c8eb6e5d06ed545
github_DeboAdeniran <raqibadeniran03@gmail.com>
2026-05-12 22:49:50 +0100

    feat: log all squad transactions

--- verified-backend/src/main/java/com/verified/config/JacksonConfig.java
+++ verified-backend/src/main/java/com/verified/config/JacksonConfig.java
@@ -1,2 +1,14 @@
-package com.verified.config;public class JacksonConfig {
+package com.verified.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
+
