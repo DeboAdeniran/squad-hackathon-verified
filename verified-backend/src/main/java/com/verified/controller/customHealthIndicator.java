927629021923a50620c48fff9667b797ce4fddb8
github_DeboAdeniran <raqibadeniran03@gmail.com>
2026-05-14 08:30:26 +0100

    feat: add custom health indicator

--- verified-backend/src/main/java/com/verified/controller/customHealthIndicator.java
+++ verified-backend/src/main/java/com/verified/controller/customHealthIndicator.java
@@ -1,13 +1,11 @@
 package com.verified.controller;
 
 
-import org.jspecify.annotations.Nullable;
 import org.springframework.boot.health.contributor.Health;
 import org.springframework.boot.health.contributor.HealthIndicator;
 import org.springframework.stereotype.Component;
 @Component
-public class HealthController implements HealthIndicator {
-
+public class customHealthIndicator implements HealthIndicator {
     @Override
     public Health health() {
         return Health.up()
